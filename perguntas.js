85c487667351b99b2da34099dd33589c25e26c18
amanda5698 <groff.amanda@escola.pr.gov.br>
2025-07-07 16:46:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao (
+    'Histórias',
+    'O que foi o holocausto?',
+    'Foi um genocídio ou assassinato em massa de cerca de 6 milhões de judeus durante a Segunda Guerra Mundial.'
+)
+
+criaCartao (
+    'Histórias',
+    'Quais as motivações para o holocausto?',
+    'O holocausto teve motivações preconceituosas como intolerâcia a religião, cor, sexulidade e origem.'
+)
+
+criaCartao (
+    'Histórias',
+    'Quais grupos eram perceguidos pelos nazistas?',
+    'Os principais grupos perceguidos eram aqueles que não seguiam os padroẽs, ou seja, judeus, negros, homossexuais, deficientes e outras minorias.'
+)
+
+criaCartao (
+    'Histórias',
+    'Os judeus tentaram lutar contra os nazistas?',
+    'Muitos judeus engajaram na resistencia armada contra os nazistas, fizeram revoltas, resistência em campos e guerra das guerrilhas.'
+)
+
+criaCartao (
+    'Histórias',
+    'O que é um campo de extermínio?',
+    'É um campo de concentração com aparatos especiais projetados específicamente para assassinatos sistemáticos.',
+)
