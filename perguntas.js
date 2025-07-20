7e855ed5077ffd94013ba579aaa45e67eee9cdb8
amanda5698 <groff.amanda@escola.pr.gov.br>
2025-07-20 13:24:47 -0300

    aula 7

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,29 @@
-criaCartao (
-    'Histórias',
+criaCartao(
+    'História',
     'O que foi o holocausto?',
     'Foi um genocídio ou assassinato em massa de cerca de 6 milhões de judeus durante a Segunda Guerra Mundial.'
 )
 
-criaCartao (
-    'Histórias',
+criaCartao(
+    'História',
     'Quais as motivações para o holocausto?',
     'O holocausto teve motivações preconceituosas como intolerâcia a religião, cor, sexulidade e origem.'
 )
 
-criaCartao (
-    'Histórias',
+criaCartao(
+    'História',
     'Quais grupos eram perceguidos pelos nazistas?',
-    'Os principais grupos perceguidos eram aqueles que não seguiam os padroẽs, ou seja, judeus, negros, homossexuais, deficientes e outras minorias.'
+    'Os principais grupos perseguidos eram aqueles que não seguiam os padroẽs, ou seja, judeus, negros, homossexuais, deficientes e outras minorias.'
 )
 
-criaCartao (
-    'Histórias',
+criaCartao(
+    'História',
     'Os judeus tentaram lutar contra os nazistas?',
     'Muitos judeus engajaram na resistencia armada contra os nazistas, fizeram revoltas, resistência em campos e guerra das guerrilhas.'
 )
 
-criaCartao (
-    'Histórias',
+criaCartao(
+    'História',
     'O que é um campo de extermínio?',
     'É um campo de concentração com aparatos especiais projetados específicamente para assassinatos sistemáticos.',
 )
